9f334091b8b4e82801889d786f439ba163650e48
dinesh-p0 <dineshkrishna0456@gmail.com>
2023-10-17 19:55:04 +0530

    new file:   components/Speaker.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Abou1 from "@/components/Abou1"
 import Abou from "@/components/Abou"
 import Abou2 from "@/components/Abou2"
 import Mobileab from "@/components/Mobileab"
+import Speaker from "@/components/Speaker"
+
+
 export default function Home() {
   return (
     <div>
     {/* <About/> */}
-    <Abou />
-    <Mobileab />
+    {/* <Abou /> */}
+    {/* <Mobileab /> */}
+    <Speaker />
      {/* <Abou2 />
     <Abou1 /> */}
     {/* <Abou /> */}
