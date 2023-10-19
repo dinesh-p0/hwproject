0d413d0461ebe3cb05bfcbd509867a00c344dbb4
dinesh-p0 <124796266+dinesh-p0@users.noreply.github.com>
2023-10-19 15:15:06 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import Abou1 from "@/components/Abou1"
 import Abou from "@/components/Abou"
+import About from "@/components/About"
 import Abou2 from "@/components/Abou2"
 import Mobileab from "@/components/Mobileab"
 import Speaker from "@/components/Speaker"
-
-
+import Guestspeaker from "@/components/Guestspeaker"
+import Partners from "@/components/Partners"
+import Scrolll from "@/components/Scrolll"
 export default function Home() {
   return (
     <div>
-    {/* <About/> */}
-    {/* <Abou /> */}
+    <About />
+    <Abou />
+    <Abou1 />
+    <Guestspeaker />
+    <Partners />
+    <Scrolll />
     {/* <Mobileab /> */}
-    <Speaker />
-     {/* <Abou2 />
-    <Abou1 /> */}
-    {/* <Abou /> */}
+    {/* <Speaker /> */}
+    {/* <Abou1 /> */}
     {/* <About/> */}
     {/* <About/> */}
     </div>
   )
 }
+
