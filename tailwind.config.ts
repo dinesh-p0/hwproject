4936969a0f12296f2702968019a86ce40124a394
dinesh-p0 <124796266+dinesh-p0@users.noreply.github.com>
2023-10-19 22:43:43 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
       },
       keyframes: {
         'infinite-scroll': {
-          from: { transform: 'translateX(100%)' },
+          from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         },
       },
