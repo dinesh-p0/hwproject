de0f133a5b7a027992b5ca261d73aea1d33e0394
dinesh-p0 <124796266+dinesh-p0@users.noreply.github.com>
2023-10-19 22:43:22 +0530

    Update Scrolll.jsx

--- components/Scrolll.jsx
+++ components/Scrolll.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scrolll = () => {
   return (
-<div className=" w-full px-6 bg-black h-[60vh] md:min-h-screen md:pt-[150px]">
+<div className=" w-full px-6 bg-black min-h-screen md:pt-[150px]">
      <div className="text-center h-auto flex flex-col items-center">
         <div className='flex flex-col md:hidden items-center gap-1'> 
             <div className='flex flex-col items-center'>
@@ -15,19 +15,34 @@ const Scrolll = () => {
         <div className='hidden md:flex pb-[40px]'>
             <p className='text-limee  text-center  text-[80px] font-bold'>OUR PARTNERS</p>
         </div>
-        <div className=" w-full flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-         <ul className="flex animate-infinite-scroll items-center justify-center ">
+        <div className=" w-full flex-nowrap py-16 flex overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+         <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
             <li className='px-8'>
-                <img src="/assets/Part1.png" width={99999} alt="StudenTribe" />
+                <img src="/assets/Part1.png" className='lg:w-[200px] md:w-[175px] w-[100px]' alt="StudenTribe" />
             </li>
             <li className='px-8'>
-                <img src="/assets/Part2.png" width={99999} alt='Git' />
+                <img src="/assets/Part2.png" className='lg:w-[200px] md:w-[175px] w-[100px]' alt='Git' />
             </li>
             <li className='px-8'>
                 <div className='flex flex-col justify-center items-center gap-2 md:gap-4'>
-                    <img src="/assets/THE.png" alt="" width={150} />
-                    <img src="/assets/Part3.png" alt="" width={99999} />
-                    <img src="/assets/LEAGUE.png" alt="" width={150} />
+                    <img src="/assets/THE.png" alt="" className='lg:w-[175px] md:w-[150px] w-[75px]'  />
+                    <img src="/assets/Part3.png" alt="" className='lg:w-[200px] md:w-[175px] w-[100px]' />
+                    <img src="/assets/LEAGUE.png" alt="" className='lg:w-[175px] md:w-[150px] w-[75px]' />
+                </div>
+            </li>
+         </ul>
+         <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+            <li className='px-8'>
+                <img src="/assets/Part1.png" className='lg:w-[200px] md:w-[175px] w-[100px]' alt="StudenTribe" />
+            </li>
+            <li className='px-8'>
+                <img src="/assets/Part2.png" className='lg:w-[200px] md:w-[175px] w-[100px]' alt='Git' />
+            </li>
+            <li className='px-8'>
+                <div className='flex flex-col justify-center items-center gap-2 md:gap-4'>
+                    <img src="/assets/THE.png" alt="" className='lg:w-[175px] md:w-[125px] w-[75px]'  />
+                    <img src="/assets/Part3.png" alt="" className='lg:w-[200px] md:w-[150px] w-[100px]' />
+                    <img src="/assets/LEAGUE.png" alt="" className='lg:w-[175px] md:w-[125px] w-[75px]' />
                 </div>
             </li>
          </ul>
