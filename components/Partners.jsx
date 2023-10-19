f0a8fa2ec685a78c661b4d6fda94a02afcd99f21
dinesh-p0 <124796266+dinesh-p0@users.noreply.github.com>
2023-10-19 20:12:35 +0530

    Update Partners.jsx

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-
+import Scrolll from './Scrolll'
 const Partners = () => {
   return (
-    <div className='bg-black min-h-screen p-8'>
-        <div className='hidden md:flex md:flex-col'>
-            <div>
-                <p className='text-limee  text-center  md:text-[80px] lg:text-[100px] font-bold'>OUR PARTNERS</p>
-            </div>
-            <div className='flex flex-row  justify-evenly items-center pt-16 p-8 '>
+    <div className='hidden lg:flex bg-black min-h-screen p-8'>
+        <div className='flex flex-col justify-center items-center'>
+            
+            <div className='flex flex-row  justify-between items-center pt-16 p-8  gap-4'>
                 <img src="/assets/Part1.png" alt="" width={250} />
                 <img src="/assets/Part2.png" alt="" width={250} />
                 <div className='flex flex-col justify-center items-center  gap-4'>
@@ -16,8 +14,7 @@ const Partners = () => {
                     <img src="/assets/LEAGUE.png" alt="" width={100} />
                 </div>
             </div>
-        </div>
-        
+        </div>        
     </div>
   )
 }
