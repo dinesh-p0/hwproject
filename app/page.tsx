4624e6ccf763fb21186901a74af09ab46caccd90
dinesh-p0 <124796266+dinesh-p0@users.noreply.github.com>
2023-10-19 20:25:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Abou1 from "@/components/Abou1"
 import Abou from "@/components/Abou"
-import About from "@/components/About"
 import Abou2 from "@/components/Abou2"
 import Mobileab from "@/components/Mobileab"
 import Speaker from "@/components/Speaker"
@@ -9,18 +8,15 @@ import Partners from "@/components/Partners"
 import Scrolll from "@/components/Scrolll"
 export default function Home() {
   return (
-    <div>
-    <About />
-    <Abou />
-    <Abou1 />
-    <Guestspeaker />
-    <Partners />
-    <Scrolll />
-    {/* <Mobileab /> */}
+    <div className="overflow-hidden">
+    <Mobileab />{/*about for sm devices */}
     {/* <Speaker /> */}
-    {/* <Abou1 /> */}
-    {/* <About/> */}
-    {/* <About/> */}
+    <Abou />
+    <Abou2 /> {/*All abou files are for md and lg devices */}
+    <Abou1 />
+    <Guestspeaker />{/*responsive*/}
+    <Scrolll />{/*responsive*/}
+
     </div>
   )
 }
